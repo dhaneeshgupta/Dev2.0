8652bd2a7e99f94fc32f9afe750e1aedbb8e5066
Hariom Gupta <hariomgupta@Harioms-MacBook-Air.local>
2026-09-08 15:56:06 +0530

    Add JavaScript practice

---
+++ JS/variable.js
@@ -0,0 +1,15 @@
+
+const accountId = 2;
+let accountEmail = "harrry@grc.com";
+let accountName = "Hariom";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+
+//accountId = 2 // not allowed to change
+console.log(accountId);
+
+accountEmail = "harry@googl.com";
+accountPassword = "3747";
+accountCity = "Jabalpur";
+
+console.table([accountEmail, accountId, accountName, accountPassword]);
