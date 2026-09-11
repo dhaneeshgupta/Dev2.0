8bb7b512670aa050935666acd1578944e335fe29
Hariom Gupta <hariomgupta@Harioms-MacBook-Air.local>
2026-09-11 17:29:11 +0530

    Add JavaScript beginnings

--- JS/dataTypes_summary.js
+++ JS/dataTypes_summary.js
@@ -33,7 +33,27 @@ console.log(typeof outSideTemp);
 console.log(typeof myFucn);
 
 
+// Memory : Stack & Heap 
+// Stack (Primitive), Heap(Non-Primitive(reference))
 
+
+let tea = "AdrakTea";
+let anotherTea = tea;
+anotherTea = "lemonTea";
+console.log(anotherTea); // o/p = LemonTea
+// Copy will be changed in Stack
+console.log(tea);
+
+let coffee ={
+    name : "ChoclateCoffee",
+    upi : "Chocolava",
+}
+
+let blackCoffee = coffee;
+// blackCoffee Object is pushed in stack, Refernece is passed of coffee
+coffee.upi = "01@ybl"; // As reference is passed so value is changed/updated
+console.log(blackCoffee.upi); // output = 01@ybl
+console.log(coffee.upi); // output = 01@ybl
 
 
 
