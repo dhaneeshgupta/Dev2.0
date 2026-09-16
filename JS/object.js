4fec094112ee1e384b790fdcead0461b56d5156b
Hariom Gupta <hariomgupta@Harioms-MacBook-Air.local>
2026-09-16 09:51:21 +0530

    Add JavaScript beginnings

--- JS/object.js
+++ JS/object.js
@@ -66,9 +66,35 @@ const regularUser = {
 
 const oBj_1 = {1: 'a', 2 : "b"};
 const oBj_2 = {3: "c", 4 : "d"};
+const oBj_4 = {5: "e", 6 : "f"};
 
 const newObj = {oBj_1, oBj_2};
 //console.log(newObj);
 
-const oBj_3 =Object.assign(oBj_1, oBj_2)
-console.log(oBj_3);
+const oBj_3 =Object.assign({}, oBj_1, oBj_2, oBj_4)
+// empty braces is treated as targeted obj and rest is source objects
+//console.log(oBj_3);
+
+
+const user = [
+    {
+        id : 1,
+        email : "h@gmail.com",
+    },
+    {
+        id : 1,
+        email : "h@gmail.com",
+    },
+    {
+        id : 1,
+        email : "h@gmail.com",
+    },
+]
+
+//console.log(user[1].email);
+//console.log(newUser);
+// console.log(Object.keys(newUser));
+// console.log(Object.values(newUser));
+// console.log(Object.entries(newUser));
+
+console.log(newUser.hasOwnProperty("isLoggedIn"));
