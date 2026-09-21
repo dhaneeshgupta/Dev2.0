65e42a7b39861c95ad315cd745894c5c2e604f61
Hariom Gupta <hariomgupta@Harioms-MacBook-Air.local>
2026-09-21 22:18:11 +0530

    Loops or Iterator

---
+++ JS/loops.js
@@ -0,0 +1,85 @@
+// looping/iteration :-
+
+// For loop :++++++++++++++++++++++++
+
+// for(let index = 0; index <=10; index++){
+//     console.log(index);
+// }
+
+
+// Print all even number from aray :
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+// for(let i = 0; i <= 20; i++){
+//     const element = i;
+//     if(i%2 == 0){
+//  console.log(element);
+//     }
+// }
+
+// console.log(element);
+
+
+// for (let i = 0; i <= 20; i++){
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best element ")
+//     }
+//     console.log(element);
+// }
+
+
+// const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [10, 11, 12]
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         if (arr[i][j] % 2 === 0) {
+//             console.log(arr[i][j]);
+//         }
+//     }
+// }
+
+// for(let i = 1; i<=5; i++){
+//     console.log(`Outer loop  is ${i}`);
+//     for(let j = 1; j<=10; j++){
+//         console.log(i + " * " + j + " = " + i*j);
+//     }
+// }
+
+// While loop/iteration :+++++++++++++++++++++++++++++=
+
+// let index = 0;
+// while(index <= 10){
+//     console.log(`Value of index ${index}`)
+//     index = index + 2;
+// }
+
+// let Myarr = ['Batman', 'Ironman', 'Superman', 'Spiderman']
+
+// let index = 0;
+// while(index < Myarr.length){
+//     console.log(`Marvel cast ${Myarr[index]}`);
+//     index+= 1;
+// }
+
+// Do while Loop : +++++++++++++++++++
+
+
+
+let index = 1;
+do{
+console.log(`Score is ${index}`)
+index ++;
+}while(index <= 10)
